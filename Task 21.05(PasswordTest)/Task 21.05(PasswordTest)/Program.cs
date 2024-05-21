b885442d39d7da225d6a93b42ea2906518648dda
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-21 12:35:59 +0400

    Task21.05

---
+++ Task 21.05(PasswordTest)/Task 21.05(PasswordTest)/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_21._05_PasswordTest_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string password = Console.ReadLine();
+            ValidatePassword(password);
+        }
+        static bool ValidatePassword(string password)
+        {
+            if (password.Length <8)
+            {
+                return false;
+            }
+            else if (!char.IsUpper(password[0])) 
+            {
+                return false;
+            }
+            else if (password.Contains("!@#$%^&*()_+-=[]|{};:<>?,. "))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
